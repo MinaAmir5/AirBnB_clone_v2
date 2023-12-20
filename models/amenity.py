684e91192341d6434066f3ada0928618df6d4259
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-12-21 01:08:43 +0200

    update

--- models/amenity.py
+++ models/amenity.py
@@ -4,12 +4,12 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """Represenation of Amnity """
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
@@ -17,5 +17,5 @@ class Amenity(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """initiaizes Ameity"""
         super().__init__(*args, **kwargs)
