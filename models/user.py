3df87396b7667cc04b2849d18d0f6c2eb53b6072
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-12-21 01:18:11 +0200

    update

--- models/user.py
+++ models/user.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
-from models.base_model import BaseModel, Base
 from os import getenv
+from models.base_model import BaseModel, Base
 import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Represetation of a user """
     if models.storage_t == 'db':
         __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
+        email = Column(String(128), nullable=False)
         reviews = relationship("Review", backref="user")
+        last_name = Column(String(128), nullable=True)
+        first_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
     else:
-        email = ""
         password = ""
-        first_name = ""
         last_name = ""
+        email = ""
+        first_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """iniializes user"""
         super().__init__(*args, **kwargs)
