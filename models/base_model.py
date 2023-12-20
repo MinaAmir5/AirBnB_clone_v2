294071937316d70cc3a1bbc7101e32490399864d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-12-21 01:41:56 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -20,14 +20,14 @@ else:
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """The BaseModel clas from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
+        created_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """Initializaton of the base model"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -44,22 +44,26 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-
-    def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+            self.created_at = datetime.utcnow()
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """updates the atribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
+    def __str__(self):
+        """String representtion of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
+    def delete(self):
+        """delete the curret instance from the storage"""
+        models.storage.delete(self)
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """returns a dictionary conaining all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -70,6 +74,3 @@ class BaseModel:
             del new_dict["_sa_instance_state"]
         return new_dict
 
-    def delete(self):
-        """delete the current instance from the storage"""
-        models.storage.delete(self)
